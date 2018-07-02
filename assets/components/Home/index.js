35278a136c7c18903ec127ae34a90751864895f5
lipouli <li.lipou@gmail.com>
2018-07-02 11:01:24 +0200

    ajout logo

--- assets/components/Home/index.js
+++ assets/components/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '~/images/click-n-send_logo.png';
+
 const Home = () => (
-  <Link to="/dashboard" href="/dashboard">
-    <button>entrer</button>
-  </Link>
+  <div>
+    <img src={logo} alt="logo" />
+    <Link to="/dashboard" href="/dashboard">
+      <button>entrer</button>
+    </Link>
+  </div>
 );
 
 export default Home;
