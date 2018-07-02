12d683032aa2c6274539f9accff91afe5507cbb9
Virginie Gruber <virginie.gruber@gmail.com>
2018-07-02 16:23:24 +0200

    added react-icon

--- assets/components/Dashboard/index.js
+++ assets/components/Dashboard/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import NavClick from '~/components/NavClick';
 
 export default () => (
   <div>
     <NavClick />
+    <Link to="/invoice" href="/invoice" className="dash-link">
+        <img  />
+        <button className="dash-button">Créer une facture</button>
+    </Link>
   </div>
 );
