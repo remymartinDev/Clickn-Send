20807d85812890dad0cd07e8960124caae430781
lipouli <li.lipou@gmail.com>
2018-07-03 11:34:42 +0200

    add factures components

---
+++ assets/components/Facture/Create.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Create = () => (
+  <div>
+    <h1>Créer votre facture</h1>
+  </div>
+);
+
+export default Create;
---
+++ assets/components/Facture/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Facture = () => (
+  <div>
+    Ici la page pour les créations de facture ?
+  </div>
+);
+
+export default Facture;
