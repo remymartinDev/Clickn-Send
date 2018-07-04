df2e7482b6e7855ea2c34054fcc50a8fcfb6dcbc
Rollo <juriens.rodrigue@gmail.com>
2018-07-04 09:20:02 +0200

    format date french style

--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -34,7 +34,7 @@ class InvoiceRepository extends ServiceEntityRepository
         $em = $this->getEntityManager();
 
         $query = $em->createQuery(
-             "SELECT DATE_FORMAT(i.date, '%Y-%m-%d') as date, i.id, i.reference, i.amount_all_taxes, i.amount_dutty_free, i.paid, DATE_FORMAT(i.deadline1, '%Y-%m-%d') as deadline1, cu.id, cu.lastname, cu.firstname, cu.company_name, s.invoice_status
+             "SELECT DATE_FORMAT(i.date, '%d-%m-%Y') as date, i.id, i.reference, i.amount_all_taxes, i.amount_dutty_free, i.paid, DATE_FORMAT(i.deadline1, '%d-%m-%Y') as deadline1, cu.id, cu.lastname, cu.firstname, cu.company_name, s.invoice_status
                 FROM App\Entity\Invoice i
                 JOIN App\Entity\Customer cu, App\Entity\Company co, App\Entity\Status s
                 WHERE i.customer = cu
