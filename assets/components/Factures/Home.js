459dd12323694240ef6e6c6fc66f0446868c8d80
lipouli <li.lipou@gmail.com>
2018-07-04 09:51:50 +0200

    add Home component for factures

---
+++ assets/components/Factures/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+  state = {
+    // factures: [],
+  }
+
+  render() {
+    return (
+      <div>
+        Affichage des factures !
+      </div>
+    );
+  }
+}
+
+export default Home;
--- assets/components/Factures/index.js
+++ assets/components/Factures/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+import Home from './Home';
 import Create from './Create';
 
 const Facture = () => (
   <Switch>
     <Route path="/invoices/create" exact component={Create} />
-    <Route path="/invoices" render={() => (<div>Page des factures</div>)} />
+    <Route path="/invoices" component={Home} />
   </Switch>
 );
 
