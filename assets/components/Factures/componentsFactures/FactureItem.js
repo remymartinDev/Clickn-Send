a9e4f4ed09086ddc5d32b754b1d9a980cae59a94
Malycia <virginie.gruber@gmail.com>
2018-07-05 16:03:50 +0200

    test

--- assets/components/Factures/componentsFactures/FactureItem.js
+++ assets/components/Factures/componentsFactures/FactureItem.js
@@ -5,6 +5,9 @@ import FaEye from 'react-icons/lib/fa/eye';
 import FaPencil from 'react-icons/lib/fa/pencil';
 import FaDownload from 'react-icons/lib/fa/download';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 import './FactureItem.scss';
 
 const FactureItem = ({
@@ -17,6 +20,9 @@ const FactureItem = ({
   const humainDate = `${formatedDate.getDate()}/${formatedDate.getMonth()}/${formatedDate.getFullYear()}`;
   return (
     <div className="facture-contain">
+      <Media query="(max-width: 769px)">
+        {matches => (matches && <FontAwesomeIcon icon={faCoffee} />)}
+      </Media>
       <div className="facture-item">{customer.pro ? customer.customerCompany : customer.lastname}</div>
       <Media query="(min-width: 769px)">
         {matches => (matches && <div className="facture-item">{humainDate}</div>)}
