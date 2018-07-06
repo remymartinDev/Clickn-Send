27a2d2e9db2ba90bd6a4ea71d443f5f14b49e10c
Malycia <virginie.gruber@gmail.com>
2018-07-06 11:19:02 +0200

    displaying invoices in RWD + add librairie React-intl + count days ago since due date

--- assets/components/Factures/componentsFactures/FactureEchue.js
+++ assets/components/Factures/componentsFactures/FactureEchue.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FaBellFull from 'react-icons/lib/fa/bell';
 import FaBell from 'react-icons/lib/fa/bell-o';
+import { FormattedRelative } from 'react-intl';
 
 import './FactureEchue.scss';
 
@@ -12,12 +13,10 @@ const FactureEchue = ({
   onClick,
   id,
 }) => {
-  const formatedDate = new Date(deadline1);
-  const humainDate = `${formatedDate.getDate()}/${formatedDate.getMonth() + 1}/${formatedDate.getFullYear()}`;
   return (
     <div className="facture-echue-contain">
       <div className="facture-echue-item">{customer.pro ? customer.customerCompany : customer.lastname}</div>
-      <div className="facture-echue-item">{humainDate}</div>
+      <div className="facture-echue-item"><FormattedRelative value={new Date(deadline1)} /></div>
       { reminder > 0 ? <FaBellFull className="bell" /> : <FaBell className="bell" onClick={onClick(id)} /> }
     </div>
   );
