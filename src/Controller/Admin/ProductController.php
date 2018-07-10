890cda82cb67083b8935547055645af3f309869d
Rollo <juriens.rodrigue@gmail.com>
2018-07-10 16:33:34 +0200

    add use in admin product

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use App\Service\ConfiguredSerializer;
 
 
 /**
