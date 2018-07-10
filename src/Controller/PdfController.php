bcd1d06c11c73fbf0f11a7d2eea8fe21ce698723
DjHHead <hhead2214@hotmail.com>
2018-07-10 17:02:14 +0200

    Route PdfController

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Invoice;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PdfController extends Controller
 {
     /**
-     * @Route("/pdf", name="pdf")
+     * @Route("/invoice/{id}/pdf", name="pdf")
      */
-    public function index()
+    public function index(Invoice $invoice)
     {
         return $this->render('pdf/index.html.twig', [
             'controller_name' => 'PdfController',
