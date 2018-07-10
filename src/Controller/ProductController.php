64753b29fb05c5aa4609672682fd09896c93a219
Rollo <juriens.rodrigue@gmail.com>
2018-07-10 13:20:54 +0200

    add serialiser for customer and product delete

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -92,7 +92,7 @@ class ProductController extends Controller
     /**
      * @Route("/{id}", name="product_delete", methods="DELETE")
      */
-    public function delete(Request $request, Product $product): Response
+    public function delete(Request $request, Product $product, SerializerInterface $serializer): Response
     {
        /*  if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) { */
             $em = $this->getDoctrine()->getManager();
