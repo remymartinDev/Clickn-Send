4475c04b4c4e2b969b865fd5130e6ffbae8c7034
Malycia <virginie.gruber@gmail.com>
2018-07-11 10:34:46 +0200

    adding product in the invoice creation

--- assets/components/Forms/Facture/Create.js
+++ assets/components/Forms/Facture/Create.js
@@ -8,112 +8,148 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import CreateClient from '~/components/Forms/Client/Create';
+import CreateProduct from '~/components/Forms/Produit/Create';
 
 import './formFacture.scss';
 
 class CreateFacture extends React.Component {
+  
   state = {
-    clients: [],
-    idNewClient: 0,
-    modal: false,
+    customers: [],
+    products: [],
+    addedProducts: [],
+    customerModal: false,
+    productModal: false,
   }
 
   componentDidMount() {
     this.getCustomers();
+    this.getProducts();
   }
 
   getCustomers = () => {
     axios.get('/api/customers')
-      .then(({data:clients}) => {
+      .then(({ data: customers }) => {
         this.setState({
-          clients,
+          customers,
         });
       });
   }
-  
-  toggle = () => {
+
+  getProducts = () => {
+    axios.get('/api/products')
+      .then(({ data: products }) => {
+        this.setState({
+          products,
+        });
+      });
+  }
+
+  getCustomersJSX = () => {
+    return this.state.customers.map((customer) => {
+      const valueModal = customer.pro ? customer.customerCompany : customer.lastname;
+      return <option key={customer.id} value={customer.id} >{valueModal}</option>;
+    });
+  }
+  getProductsJSX = () => {
+    return this.state.products.map((product) => {
+      return <option key={product.id} value={product.id} >{product.denomination}</option>;
+    });
+  }
+    
+  toggle = modal => () => {
+    console.log('toggle modal', this.state[modal], modal);
     this.setState({
-      modal: !this.state.modal,
+      [modal]: !this.state[modal],
     });
   }
 
-  getClientJSX = () => {
-    return this.state.clients.map((client) => {
-      const valueModal = client.pro ? client.customerCompany : client.lastname;
-      return <option key={client.id} value={client.id} >{valueModal}</option>;
-    });
-  }
-
-  submit = (values) => {
+  customerSubmit = (values) => {
     axios.post('/api/customer/new', values)
       .then((response) => {
-        console.log(response);
-        if(response.data.succes) {
-          this.toggle();
+        if (response.data.succes) {
+          this.toggle('customerModal')();
           this.props.changeCustomers(response.data.id, this.getCustomers);
-          // this.setState({
-          //   idNewClient: response.data.id,
-          // }, this.getCustomers);
-          console.log(this.state.idNewClient);
         }
       });
   }
-  toggleValue = () => {
-    console.log('in toggle value');
-    this.setState({
-      idNewClient: 'toctoc',
-    });
-  }
+  productSubmit = (values) => {
+    axios.post('/api/product/new', values)
+      .then((response) => {
+        if (response.data.succes) {
+          this.toggle('productModal')();
+          this.props.changeProducts(response.data.id, this.getProducts);
+        }
+      });
+  }        
 
-  selectHandler = (e) => {
-    const { value } = e.target;
-    console.log(value);
-    this.setState({
-      idNewClient: value,
-    });
-  }
-
-  invoiceCreateSubmit = values => {
-    console.log('mon submit', values);
-  }      
-  
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit}>
-          <label htmlFor="selectClient">Client</label>
-          <h1>{this.state.idNewClient}</h1>
-          <Field component="select" name="selectClient">
-            <option>Sélectionnez votre client</option>
-            {this.getClientJSX()}
-            <option value="toctoc">toctoc</option>
-            <option value={3}>tactac</option>
-          </Field>
-          <Button onClick={this.toggle} className="modal-button">
-            <FontAwesomeIcon className="modal-icon" icon={faPlus} />
-          </Button>
-          <Button type='submit'>submit invoice</Button>
+          <div className="add-client">
+            <label htmlFor="customerId">Client</label>
+            <h1>{this.state.idNewClient}</h1>
+            <Field component="select" name="customerId">
+              <option>Sélectionnez votre client</option>
+              {this.getCustomersJSX()}
+            </Field>
+            <Button onClick={this.toggle('customerModal')} className="modal-button">
+              <FontAwesomeIcon className="modal-icon" icon={faPlus} />
+            </Button>
+            <Modal isOpen={this.state.customerModal} toggle={this.toggle('customerModal')}>
+              <ModalHeader toggle={this.toggle('customerModal')}>Créez votre client</ModalHeader>
+              <ModalBody>
+                <CreateClient onSubmit={this.customerSubmit} />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={this.toggle('customerModal')}>Créer</Button>
+                <Button color="secondary" onClick={this.toggle('customerModal')}>Annuler</Button>
+              </ModalFooter>
+            </Modal>
+          </div>
+          <div className="add-produit">
+            <label htmlFor="productId">Produit</label>
+            <h1>{this.state.idNewClient}</h1>
+            <Field component="select" name="productId">
+              <option>Sélectionnez votre produit</option>
+              {this.getProductsJSX()}
+            </Field>
+            <Button onClick={this.toggle('productModal')} className="modal-button">
+              <FontAwesomeIcon className="modal-icon" icon={faPlus} />
+            </Button>
+            <Modal isOpen={this.state.productModal} toggle={this.toggle('productModal')}>
+              <ModalHeader toggle={this.toggle('productModal')}>Créez votre produit</ModalHeader>
+              <ModalBody>
+                <CreateProduct onSubmit={this.productSubmit} />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={this.toggle('productModal')}>Créer</Button>
+                <Button color="secondary" onClick={this.toggle('productModal')}>Annuler</Button>
+              </ModalFooter>
+            </Modal>
+            <Field component="text" name="quantity"></Field>
+          </div>
+          <Button type="submit">Générer votre facture</Button>
         </form>
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}>Créez votre client</ModalHeader>
-          <ModalBody>
-            <CreateClient onSubmit={this.submit} />
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Créer</Button>
-            <Button color="secondary" onClick={this.toggle}>Annuler</Button>
-          </ModalFooter>
-        </Modal>
-      </div> 
+      </div>
     );
   }
 }
 
-CreateFacture.propTypes = {};
+CreateFacture.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  changeCustomers: PropTypes.func.isRequired,
+  changeProducts: PropTypes.func.isRequired,
+};
 
 const mapDispatchToProps = dispatch => ({
   changeCustomers: (id, callback) => {
-    dispatch(change('facture', 'selectClient', id));
+    dispatch(change('facture', 'customerId', id));
+    callback();
+  },
+  changeProducts: (id, callback) => {
+    dispatch(change('facture', 'productId', id));
     callback();
   },
 });
