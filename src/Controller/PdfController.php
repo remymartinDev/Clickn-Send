74fe6479c471bdb29e90c2979774910effba8c72
DjHHead <hhead2214@hotmail.com>
2018-07-11 12:33:07 +0200

    custom query pdf twig

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,11 +12,14 @@ class PdfController extends Controller
     /**
      * @Route("/invoice/{id}/pdf", name="pdf")
      */
-    public function index(Invoice $invoice)
+    public function index(Invoice $invoice, InvoiceRepository $invoiceRepo)
     {
+
+        $invoices = $invoiceRepo->findForPdf($invoice); 
+
         return $this->render('pdf/index.html.twig', [
-            'controller_name' => 'PdfController',
             'title' => 'Facture PDF',
+            'invoices' => $invoices,
         ]);
     }
 }
