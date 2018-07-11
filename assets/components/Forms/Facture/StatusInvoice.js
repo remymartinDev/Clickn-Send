20b0c0578e3de9e16d7e94e835b7c573e052d48d
lipouli <li.lipou@gmail.com>
2018-07-11 14:47:50 +0200

    add StatusInvoice.js

---
+++ assets/components/Forms/Facture/StatusInvoice.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Axios from 'axios';
+import { Field } from 'redux-form';
+
+class StatusInvoice extends React.Component {
+  state = {
+    status: [],
+  }
+
+  componentDidMount() {
+    Axios.get('/api/status')
+      .then(({ data: status }) => {
+        this.setState({
+          status,
+        });
+      });
+    this.getStatusJSX();
+  }
+
+  getStatusJSX = () => (
+    this.state.status.map(({ id, invoiceStatus }) => (
+      <option key={id}>{invoiceStatus}</option>
+    ))
+  )
+
+  render() {
+    return (
+      <Field component="select">
+        {this.getStatusJSX}
+      </Field>
+    );
+  }
+}
+
+export default StatusInvoice;
