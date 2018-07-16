ae86af3a71d651df47e3104d586a1c1b75120be8
Malycia <virginie.gruber@gmail.com>
2018-07-16 15:53:25 +0200

    creating user

---
+++ assets/components/Forms/Signup/InputFile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputFile = () => (
+  <input type="file" />
+);
+
+export default InputFile;
--- assets/components/Invoices/Create.js
+++ assets/components/Invoices/Create.js
@@ -4,26 +4,6 @@ import PropTypes from 'prop-types';
 import CreateFacture from '~/components/Forms/invoice/Create';
 import Modal from './componentsInvoices/Modal';
 
-// class Create extends React.Component {
-
-// creatInvoice = (values) => {
-//   console.log('insubmit invoice', values);
-// axios.post('/api/invoice/new', values)
-//   .then((response) => {
-//     console.log(response.data);
-//   });
-// }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <CreateFacture onSubmit={this.props.createInvoice} />;
-//         <Modal />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const Create = ({ createInvoice }) => (
   <React.Fragment>
     <CreateFacture onSubmit={createInvoice} />;
