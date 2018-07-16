389a50fc8928f926c46d5773acf31a9ea99d6c6c
Rollo <juriens.rodrigue@gmail.com>
2018-07-16 16:23:28 +0200

    install security and fosuser bunddle

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+    }
+}
