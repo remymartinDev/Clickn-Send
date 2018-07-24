12bfa5da6528eb06bc021a378a97c9f7095db6b7
lipouli <li.lipou@gmail.com>
2018-07-24 19:33:31 +0200

    addd username in dashboard

--- assets/components/Dashboard/index.js
+++ assets/components/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import ButtonCreate from '~/components/ButtonCreate';
 
 import './dashboard.scss';
 
-export default () => (
+const Dashboard = ({ userConnected: { username } }) => (
   <div className="contain-dash">
-    <h1 className="titre"> Bienvenue username </h1>
+    <h1 className="titre"> Bienvenue {username} </h1>
     <div className="dash-links">
       <ButtonCreate type="facture" />
       <ButtonCreate type="produit" />
@@ -14,3 +16,16 @@ export default () => (
     </div>
   </div>
 );
+
+Dashboard.propTypes = {
+  userConnected: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  userConnected: state.notreReducer.userConnected ? state.notreReducer.userConnected : {},
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(Dashboard);
